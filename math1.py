dd59357278d14982c597ab6fa5c20ba4b0818d64
ch_s <100966925+cem-lab@users.noreply.github.com>
2023-07-19 11:58:26 +0300

    Create math1.py

---
+++ math1.py
@@ -0,0 +1,18 @@
+import math ### Imports the math plugin.
+
+pi = 3.14159265 ### I set some numbers here not really much to look.
+x = 1
+y = 2
+z = 3
+e = 4
+
+print(round(pi)) ### Yuvarlama - Rounding
+print(math.ceil(pi)) ### Yukarıya Yuvarlama - Ceiling? ( this is not a verb )
+print(math.floor(pi)) ### Aşağıya Yuvarlama - Flooring
+print(abs(pi)) ### Sıfıra Uzaklık- Absolute Value
+print(math.pow(pi,2)) ### Pow-er
+print(math.sqrt(pi)) ### Square root
+print(max(z,y,z,e)) ### Tells the biggest number in the terminal
+print(min(x,y,z,e)) ### Tells the smallest number in the terminal
+
+##### You cant work with running all of the code at the same time. And im letting you know that this is some of the math module futures. #####
